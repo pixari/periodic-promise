0ade11f802bbc6edcb0e54968219358fa333e0ee
Raffaele Pizzari <raffaele.pizzari@gmail.com>
2019-07-15 13:23:33 +0200

    Fixes wrong package name

--- rollup.config.js
+++ rollup.config.js
@@ -8,7 +8,7 @@ export default [
   {
     input: 'src/index.ts',
     output: {
-      name: 'periodc-promise',
+      name: 'periodic-promise',
       file: pkg.browser,
       format: 'umd',
     },
